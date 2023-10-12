8a36acc26c323ebd9605c89691e659065f80a369
Svetlana <A.svet.mr@yandex.ru>
2023-10-12 15:56:56 +0300

    v4 ввод из дока/ не робит

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+// ввод из дока
+        int[] massiv= readArrayFromF("number_array.txt");
+        int countD = countDistinct(massiv);
+        System.out.println("в массиве "+countD+" разных чисел");
+        System.out.println(Arrays.toString(massiv));
+//
         System.out.printf("Введите длинну массива: ");
         Scanner scanner = new Scanner(System.in);
         int[] mass = new int[scanner.nextInt()];//{1, 2, 1, 2, 4, 5};
@@ -42,5 +50,32 @@ public class Main {
     public static int uniqueCountStream(int[] mass)
     {
         return (int) Arrays.stream(mass).distinct().count();
+    }
+
+//Ввод чисел из документа /не робит
+    public static int countDistinct(int[] mas) {
+        int count = 0;
+        for (int i = 0; i < mas.length; i++) {
+            int y = mas[i];
+            if (!isPresent(y, mas, 0, i - 1))
+                count++;
+        }
+        return count;
+    }
+    public static boolean isPresent(int x, int[] mas, int from, int to){
+        for (int i = from; i<=to; i++)        {
+            if(mas[i] == x)
+                return true;
+        }
+        return false;
+    }
+    private static int[] readArrayFromF(String fileName) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(fileName));
+        int n = scan.nextInt();
+        int[] mas = new int[n];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = scan.nextInt();
+        }
+        return mas;
     }
 }
